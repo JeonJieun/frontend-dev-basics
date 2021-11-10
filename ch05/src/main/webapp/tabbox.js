03c70ee43d941b1131e0fb3fce3ce825f3e17877
JeonJieun <wjn135@naver.com>
2021-11-10 17:52:47 +0900

    20211110 수업

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -3,23 +3,23 @@
 
 var tabBox = {
 	init: function() {
-		window.addEventListener("load", this.onWindowLoad.bind(this));
+		window.addEventListener("load", this._onWindowLoad.bind(this));
 	},
-	onTabClicked: function() {
+	_onTabClicked: function() {
 		// unselect
 		var lisSelected = document.getElementsByClassName("selected");
 		(lisSelected.length === 1) && (lisSelected[0].className = '');
-
+		
 		// select
 		this.className = "selected";
 	},
-	onWindowLoad: function() {
+	_onWindowLoad: function() {
 		var divTabBox = document.getElementsByClassName("tab-box")[0];
 		var ul = divTabBox.childNodes[1];
 
 		var liTabs = ul.getElementsByTagName("li");
 		for(var i = 0; i < liTabs.length; i++) {
-			liTabs[i].addEventListener("click", this.onTabClicked);
+			liTabs[i].addEventListener("click", this._onTabClicked);
 		}		
 	}
-} 
+}
